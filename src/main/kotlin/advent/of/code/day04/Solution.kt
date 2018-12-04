6e38667bde0856056a1a7698b8969228018acd07
Bruno Rene Santos <bruno.santos@sky.uk>
2018-12-04 14:23:00 +0000

    day 4 - enhanced v2

--- src/main/kotlin/advent/of/code/day04/Solution.kt
+++ src/main/kotlin/advent/of/code/day04/Solution.kt
@@ -4,9 +4,9 @@ import advent.of.code.day04.State.*
 import java.io.File
 
 enum class State(val match: Regex) {
-    SHIFT(Regex("\\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})\\] Guard #([0-9]+) begins shift")),
-    SLEEP(Regex("\\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})\\] falls asleep")),
-    AWAKE(Regex("\\[([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2})\\] wakes up"))
+    SHIFT(Regex(".+Guard #([0-9]+) begins shift")),
+    SLEEP(Regex(".+:([0-9]{2})\\] falls asleep")),
+    AWAKE(Regex(".+:([0-9]{2})\\] wakes up"))
 }
 
 fun commonPart(selector: (Map.Entry<Int, Array<Int>>) -> Int): Int {
@@ -17,15 +17,15 @@ fun commonPart(selector: (Map.Entry<Int, Array<Int>>) -> Int): Int {
     for (line in lines) {
         when {
             SHIFT.match matches line -> {
-                currentGuard = SHIFT.match.find(line)?.groups!![6]!!.value.toInt()
+                currentGuard = SHIFT.match.find(line)?.groups!![1]!!.value.toInt()
                 if (guardSleep[currentGuard] == null)
                     guardSleep[currentGuard] = Array(60) { 0 }
             }
             SLEEP.match matches line -> {
-                asleep = SLEEP.match.find(line)!!.groups[5]!!.value.toInt()
+                asleep = SLEEP.match.find(line)!!.groups[1]!!.value.toInt()
             }
             AWAKE.match matches line -> {
-                val awake = AWAKE.match.find(line)!!.groups[5]!!.value.toInt()
+                val awake = AWAKE.match.find(line)!!.groups[1]!!.value.toInt()
                 for (b in (asleep until awake))
                     guardSleep[currentGuard]!![b]++
             }
